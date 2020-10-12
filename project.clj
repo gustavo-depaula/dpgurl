370d0663adde976421e7a99940c1c9f2d7e3650f
Gustavo de Paula <gustavodepaula@disroot.org>
2020-10-12 14:23:01 -0300

    feat: add redis

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-json "0.3.1"]
+                 [com.taoensso/carmine "3.0.0"]
                  [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler dpgurl.handler/app}
--- src/dpgurl/db.clj
+++ src/dpgurl/db.clj
@@ -1,14 +1,44 @@
-(ns dpgurl.db)
+(ns dpgurl.db
+  (:require [taoensso.carmine :as car :refer [wcar]]))
 
+(def redis-url (System/getenv "REDIS_URL"))
+
+;; Local, in-memory db if REDIS_URL not present
+;; no support for expirable links
 (def db (atom {}))
 
-(defn get-long-url [short]
+(defn- local-db-get-long-url [short]
   (let [db-value (deref db)
         long-url (get db-value short)]
     long-url))
 
-(defn insert-url-into-db [short long]
+(defn- local-db-insert-url-into-db [short long]
     (swap!
      db
      #(assoc %1 short long)))
-  
+
+;; Redis db
+;; uses carmine https://github.com/ptaoussanis/carmine
+(def server1-conn {:pool {} :spec {:uri redis-url}})
+(defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
+
+(defn- redis-get-long-url [short]
+  (wcar* (car/get short)))
+
+(def day-in-seconds (* 24 60 60))
+(def expire-short-url-time day-in-seconds)
+(defn- redis-insert-url-into-db [short long]
+  (wcar*
+   (car/set short long)
+   (car/expire short expire-short-url-time)))
+
+;; public interface
+(defn get-long-url [short]
+  (if (nil? redis-url)
+    (local-db-get-long-url short)
+    (redis-get-long-url short)))
+
+(defn insert-url-into-db [short long]
+  (if (nil? redis-url)
+    (local-db-insert-url-into-db short long)
+    (redis-insert-url-into-db short long)))
